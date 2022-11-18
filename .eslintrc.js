e043cdc31b635dccb289254a714d79f58ce4e0b7
Mufaqar Islam <mufaqar@gmail.com>
2022-11-18 20:43:06 +0500

    rules type

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,29 @@
 /* eslint-disable no-undef */
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
-    'eslint:recommended',
-    'plugin:react/recommended',
-  ],
-  'settings': {
-    'react': {
-      'version': 'latest',
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  settings: {
+    react: {
+      version: 'latest',
     },
   },
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'ecmaVersion': 12,
-    'sourceType': 'module',
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
-  'plugins': [
-    'react',
-  ],
-  'rules': {
-    'indent': [
-      'error',
-      2,
-      { 'SwitchCase': 1 },
-    ],
+  plugins: ['react'],
+  rules: {
+    'react/prop-types': 'off',
+    indent: ['error', 2, { SwitchCase: 1 }],
     'linebreak-style': 0,
-    'quotes': [
-      'error',
-      'single',
-    ],
-    'semi': [
-      'error',
-      'always',
-    ],
-    'comma-dangle': [
-      'error',
-      'only-multiline',
-    ],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'only-multiline'],
   },
 };
