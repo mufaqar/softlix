46c78886d4ca88d94f8b7196a6c24eaa78fc5e7c
Mufaqar Islam <mufaqar@gmail.com>
2022-11-18 18:42:47 +0500

    Delete 10-tech-trends-that-will-influence-your-marketing-2022.js

--- src/pages/blog/10-tech-trends-that-will-influence-your-marketing-2022.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import TechTrendsThatWillInfluence from 'views/TechTrendsThatWillInfluence';
-
-const TechTrendsThatWillInfluencePage = () => {
-  return <TechTrendsThatWillInfluence />;
-};
-
-export default TechTrendsThatWillInfluencePage;
