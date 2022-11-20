18dfe75bd29e67e0407b000a33059308ef768041
Mufaqar Islam <mufaqar@gmail.com>
2022-11-20 15:06:55 +0500

    Featured Logos

--- src/views/IndexView/components/LogoGridSimpleCentered/LogoGridSimpleCentered.js
+++ src/views/IndexView/components/LogoGridSimpleCentered/LogoGridSimpleCentered.js
@@ -8,11 +8,7 @@ import Container from 'components/Container';
 const mock = [
   '/images/clients/client1.png',
   '/images/clients/client2.png',
-  '/images/clients/client3.png',
-  '/images/clients/client4.png',
-  '/images/clients/client5.png',
   '/images/clients/client6.png',
-  '/images/clients/client7.png',
   '/images/clients/client8.png',
   '/images/clients/client9.png',
   '/images/clients/client10.png',
