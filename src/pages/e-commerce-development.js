54fd6d1800b22ffb85f6bb0a7dad9680da30ded0
Mufaqar Islam <mufaqar@gmail.com>
2022-11-22 18:04:55 +0500

    Header

--- src/pages/e-commerce-development.js
+++ src/pages/e-commerce-development.js
@@ -1194,8 +1194,8 @@ export default function EcommercePage() {
                       y2="-106.425"
                       gradientUnits="userSpaceOnUse"
                     >
-                      <stop stop-color="white" stop-opacity="0.07" />
-                      <stop offset="1" stop-color="white" stop-opacity="0" />
+                      <stop stopColor="white" stopOpacity="0.07" />
+                      <stop offset="1" stopColor="white" stopOpacity="0" />
                     </linearGradient>
                   </defs>
                 </svg>
@@ -1224,8 +1224,8 @@ export default function EcommercePage() {
                       y2="88.5747"
                       gradientUnits="userSpaceOnUse"
                     >
-                      <stop stop-color="white" stop-opacity="0.07" />
-                      <stop offset="1" stop-color="white" stop-opacity="0" />
+                      <stop stopColor="white" stopOpacity="0.07" />
+                      <stop offset="1" stopColor="white" stopOpacity="0" />
                     </linearGradient>
                   </defs>
                 </svg>
